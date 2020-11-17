ceac9455e15092846cd3e122c6482e71e0a4b952
deepcloudlabs <deepcloudlabs@gmail.com>
2020-11-17 11:06:25 +0300

    explain functional programming and stream api

--- core-banking/src/com/example/banking/app/BankingApp.java
+++ core-banking/src/com/example/banking/app/BankingApp.java
@@ -1,5 +1,9 @@
 package com.example.banking.app;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import com.example.banking.domain.Account;
 import com.example.banking.domain.Customer;
 
 public class BankingApp {
@@ -16,5 +20,25 @@ public class BankingApp {
           System.out.println(jack);
           System.out.println(jack.toString());
           System.out.println(jack.getClass());
+          jack.addAccount(new Account("tr1", 10_000));
+          jack.addAccount(new Account("tr2", 20_000));
+          jack.addAccount(new Account("tr3", 30_000));
+          System.out.println("Number of accounts is "+jack.getNumberOfAccounts());
+          System.out.println(jack.getAccount(1));
+          jack.getAccount("tr4").ifPresent( account -> account.withdraw(1_000) );
+          jack.getAccount("tr4").ifPresent( account -> account.withdraw(1_000) );
+          
+          // Alt + Shift + L -> Extract to Local variable
+          // Lambda Expression
+          Consumer<Account> withdraw1k = account -> account.withdraw(1_000);
+          Consumer<Account> printAccount = System.out::println;
+		  jack.getAccount("tr2").ifPresent( withdraw1k.andThen(printAccount) );
+		  Optional<Account> acc = jack.getAccount("tr2");
+		  if (acc.isPresent()) {
+			  acc.get().withdraw(1_000);
+			  System.out.println(acc.get());
+		  }
+		  System.out.println("Total balance: "+jack.getBalance());
+		  System.out.println("Total balance8: "+jack.getBalance8());
 	}
 }
--- core-banking/src/com/example/banking/domain/Customer.java
+++ core-banking/src/com/example/banking/domain/Customer.java
@@ -1,12 +1,22 @@
 package com.example.banking.domain;
-// Alt + Shift + S : Generate Source Code
-public class Customer extends Object { // Class -> i. attribute, ii. behaviour -> method
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.ToDoubleFunction;
+
+// Alt + Shift + S : Generate Source Code     Alt+Insert
+// Ctrl + Shift + F : Format Code             Ctrl + Alt + format(L)a
+// Ctrl + Shift + O : Organize Import         Ctrl + Alt + I
+public class Customer { // Class -> i. attribute, ii. behaviour -> method
 	private final String identity; // attribute/state/data
 	private String fullname;
 	private final int birthYear;
 	private String email;
 	private String sms;
-	//Constructor -> method -> initializes attributes
+	private List<Account> accounts = new ArrayList<>();
+
+	// Constructor -> method -> initializes attributes
 	public Customer(String identity, String fullname, int birthYear, String email, String sms) {
 		this.identity = identity;
 		this.fullname = fullname;
@@ -14,36 +24,79 @@ public class Customer extends Object { // Class -> i. attribute, ii. behaviour -
 		this.email = email;
 		this.sms = sms;
 	}
+
 	public String getFullname() {
 		return fullname;
 	}
+
 	public void setFullname(String fullname) {
 		this.fullname = fullname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSms() {
 		return sms;
 	}
+
 	public void setSms(String sms) {
 		this.sms = sms;
 	}
+
 	public String getIdentity() {
 		return identity;
 	}
+
 	public int getBirthYear() {
 		return birthYear;
 	}
+
+	public void addAccount(Account account) {
+		accounts.add(account);
+	}
+	
+	public int getNumberOfAccounts() {
+		return accounts.size();
+	}
+	
+	public Account getAccount(int index) { // Overloading
+	    return accounts.get(index);	
+	}
+	
+	public Optional<Account> getAccount(String iban) { // Overloading
+		for (Account acc : accounts) { // for-each
+			if (acc.getIban().equals(iban)) return Optional.of(acc);
+		}
+		return Optional.empty();
+	}
+	
+	public double getBalance() {
+		double balance = 0.0;
+		for (Account account : accounts)
+			balance = balance + account.getBalance();
+		return balance;
+	}
+	public double getBalance8() {
+		return accounts.stream()
+				       .parallel()
+				       .filter(account -> account.getBalance()>0.0)
+//				       .map(Account::getBalance)
+				       .mapToDouble(Account::getBalance)
+				       //.reduce(0.0,(a,balance)->a+balance);
+//				       .reduce(0.0,Double::sum);
+				       .sum();
+	}
+	
 	@Override
 	public String toString() {
 		return "Customer [identity=" + identity + ", fullname=" + fullname + ", birthYear=" + birthYear + ", email="
 				+ email + ", sms=" + sms + "]";
 	}
 
-	
-	
 }
